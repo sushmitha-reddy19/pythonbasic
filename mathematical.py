6a88e096607bf6e38e0174665159ccd89d6938cf
sushmitha-reddy19 <sushmith1353@gmail.com>
2025-12-30 21:02:27 +0530

    initial commit

---
+++ mathematical.py
@@ -0,0 +1,10 @@
+a=int(input("enter a number"))
+b=int(input("enter 2nd number"))
+addition=a+b
+subtraction=a-b
+multiplication=a*b
+division=a/b
+print("addition:",addition)
+print("subtraction:",subtraction)
+print("multiplication",multiplication)
+print("division",division)
---
+++ name.py
@@ -0,0 +1,4 @@
+first_name=input("enter first name")
+last_name=input("enter last name")
+name=first_name+last_name
+print("Hello", name ,"welcome to python program")
